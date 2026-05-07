d33c6bb3d9081f274115e6732ccbc2c34313b927
sammy-9930 <samanthamenezes99@gmail.com>
2026-05-06 23:02:18 -0400

    Adding proto files

--- src/shippingservice/config.py
+++ src/shippingservice/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI     = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "shipping_service")
+MONGO_URI     = os.getenv("MONGO_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "shipping_agent")
